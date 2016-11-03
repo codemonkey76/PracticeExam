e9662a53e2201bba055440e01f1743f0eefb691b
Shane Poppleton <shane@bjja.com.au>
2016-11-03 17:22:20 +1000

    Added text about signing up

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
     <div class="jumbotron">
         <h1>Practice Test</h1>
         <p>To do a practice test, or create your own, click the link below.</p>
-
+        <p>You can only try a practice test or create your own test if you have registered for an account and have signed in</p>
         <p>
             <a class="btn btn-lg btn-primary" href="exam" role="button">Open Tests</a>
         </p>
