77f3eae247995b8b489817e327248fa02062306c
Shane Poppleton <shane@bjja.com.au>
2016-11-06 18:40:20 +1000

    Added Chat Widget

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,19 @@
         ]); ?>
     </script>
 </head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/581eeb6fe808d60cd07b203a/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
